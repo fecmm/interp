8f30496c723068abe8c2d4cec4ad995fc1b15cb0
fecmm <fcmm@ic.ufal.br>
2025-10-28 20:46:23 -0300

    Update main.py

--- interface/main.py
+++ interface/main.py
@@ -1,31 +1,50 @@
-from fastapi import FastAPI, Form, Request
+# main.py
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
+from jinja2 import Environment, FileSystemLoader
+import os
 
+
+from minipar.lexer_251018_215612 import Lexer
+from minipar.parser_251018_215706 import Parser
 from minipar.interpreter_3000 import Interpreter
-from minipar.parser_251018_215706 import parse  
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
 
+templates_path = os.path.join(os.path.dirname(__file__), "interface", "templates")
+env = Environment(loader=FileSystemLoader(templates_path))
+
+
+app.mount("/static", StaticFiles(directory=os.path.join("interface", "static")), name="static")
 
 @app.get("/", response_class=HTMLResponse)
-async def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request, "result": ""})
-
+async def index():
+    template = env.get_template("index.html")
+    return template.render()
 
 @app.post("/run", response_class=HTMLResponse)
-async def run_code(request: Request, code: str = Form(...)):
+async def run_code(code: str = Form(...)):
     try:
-        # parse retorna a AST do código MiniPar
-        ast = parse(code)
-        interpreter = Interpreter()
-        interpreter.interpret(ast)
-        output = "Código executado com sucesso."
-    except Exception as e:
-        output = f"Erro: {str(e)}"
+        
+        lexer = Lexer(code)
+        tokens = lexer.tokenize()  
 
-    return templates.TemplateResponse("index.html", {"request": request, "result": output})
+      
+        parser = Parser(tokens)
+        ast = parser.parse()  
+
+      
+        interpreter = Interpreter()
+        output = interpreter.run(ast)  
+
+    except Exception as e:
+        output = f"Erro: {e}"
+
+    template = env.get_template("index.html")
+    return template.render(code=code, output=output)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
