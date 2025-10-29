180dc29f34f83b5ef2ed91ae4b12277226db3543
fecmm <fcmm@ic.ufal.br>
2025-10-28 22:22:27 -0300

    Update main.py

--- interface/main.py
+++ interface/main.py
@@ -1,50 +1,31 @@
-# main.py
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from jinja2 import Environment, FileSystemLoader
-import os
 
 
 from minipar.lexer_251018_215612 import Lexer
 from minipar.parser_251018_215706 import Parser
-from minipar.interpreter_3000 import Interpreter
 
 app = FastAPI()
+templates = Jinja2Templates(directory="interface/templates")
 
-
-templates_path = os.path.join(os.path.dirname(__file__), "interface", "templates")
-env = Environment(loader=FileSystemLoader(templates_path))
-
-
-app.mount("/static", StaticFiles(directory=os.path.join("interface", "static")), name="static")
+app.mount("/static", StaticFiles(directory="interface/static"), name="static")
 
 @app.get("/", response_class=HTMLResponse)
-async def index():
-    template = env.get_template("index.html")
-    return template.render()
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
 
 @app.post("/run", response_class=HTMLResponse)
-async def run_code(code: str = Form(...)):
+def run_code(request: Request, code: str = Form(...)):
     try:
-        
         lexer = Lexer(code)
-        tokens = lexer.tokenize()  
-
-      
+        tokens = lexer.tokenize()
         parser = Parser(tokens)
-        ast = parser.parse()  
-
-      
-        interpreter = Interpreter()
-        output = interpreter.run(ast)  
-
+        ast = parser.parse_program()
+        result = str(ast)  # ou outra forma de visualizar o AST
+        return templates.TemplateResponse("index.html", {"request": request, "result": result, "code": code})
     except Exception as e:
-        output = f"Erro: {e}"
+        return templates.TemplateResponse("index.html", {"request": request, "result": f"Error: {e}", "code": code})
 
-    template = env.get_template("index.html")
-    return template.render(code=code, output=output)
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
